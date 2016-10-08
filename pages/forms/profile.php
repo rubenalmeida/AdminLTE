c5773937d5e0ddba10747eed143b1e00cdf4d757
15114290019 <15114290019@OLAB6-D08.academico.iesb.br>
2016-10-07 21:54:07 -0300

    erros na listagem de clientes 2

--- pages/forms/profile.php
+++ pages/forms/profile.php
@@ -1,14 +1,14 @@
 <?php include_once "../../menu.php"?>
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
-
 include_once '../../administrador/livros/emprestimos/teste_emprestimos.php';
 
 $livros = new Emprestar();
 $livros = $livros->listarEmprestados($id_cliente);
+
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 
 
 ?>
@@ -42,7 +42,7 @@ $livros = $livros->listarEmprestados($id_cliente);
 
               <ul class="list-group list-group-unbordered">
                 <li class="list-group-item">
-                  <b>Livros Emprestados</b> <a class="pull-right"><?php ############################################ ?></a>
+                  <b>Livros Emprestados</b> <a class="pull-right"><?php ?></a>
                 </li>
               </ul>
 
