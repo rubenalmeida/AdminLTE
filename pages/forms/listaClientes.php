4992b7d16757ae7b118149cb2306d59c68dc00ec
15114290019 <15114290019@OLAB6-D08.academico.iesb.br>
2016-10-07 22:02:25 -0300

    erros na listagem de clientes 7

--- pages/forms/listaClientes.php
+++ pages/forms/listaClientes.php
@@ -6,6 +6,10 @@ $emprestimo = new Emprestar();
 $emprestimo1 = $emprestimo->recuperarClientes();
 
 $emprestimo2 = $emprestimo->recuperarInativos();
+
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 ?>
 
 <div class="content-wrapper">
